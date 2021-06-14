d8aa73e043443c4f616d1190785ab63c443ae455
Sergey <GreyTob@inbox.ru>
2021-06-14 12:04:00 +0500

    add routing, Jumbotron

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
     <div className="navbar-brand">Github search</div>
     <ul className="navbar-nav">
       <li className="navbar-item">
-        <a href="/" className="nav-link">
+        <NavLink to="/" className="nav-link">
           Main
-        </a>
+        </NavLink>
       </li>
       <li className="navbar-item">
-        <a href="/about" className="nav-link">
+        <NavLink to="/about" className="nav-link">
           Information
-        </a>
+        </NavLink>
       </li>
     </ul>
   </nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
-export const Home = () => {
-  return (
-    <div>
-      <h3>Home page</h3>
-    </div>
-  )
-}
+import img1 from '../assets/IMG_3874.jpg'
+import img2 from '../assets/IMG_40331.jpg'
+import img3 from '../assets/mc-helios-44-m-7-58-mm-2-sample-14.jpg'
+
+export const Home = () => (
+  <Jumbotron fluid>
+    <h1>Привет Мир</h1>
+    <p>Это компонент Jumbotron продклченный с помощью react-bootstrap</p>
+    <p>
+      <Button variant="primary">Learn more</Button>
+    </p>
+  </Jumbotron>
+)
