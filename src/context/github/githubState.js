26ca42da061acf10258313472909295661869523
Sergey <GreyTob@inbox.ru>
2021-06-18 14:03:25 +0500

    guthub state part 2

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,4 +1,11 @@
 import React, { useReduser } from 'core-js/library/fn/reflect/es7/metadata'
+import {
+  CLEAR_USERS,
+  GET_REPOS,
+  GET_USER,
+  SEARCH_USERS,
+  SET_LOADING,
+} from '../tupes'
 import { GithubContext } from './githubContext'
 import { githubReducer } from './githubReducer'
 
@@ -11,5 +18,57 @@ export const GithubState = ({ children }) => {
   }
   const [state, dispatch] = useReduser(githubReducer, initialState)
 
-  retunr(<GithubContext.Provider value={{}}>{children}</GithubContext.Provider>)
+  const search = async (value) => {
+    setLoading()
+    //axios
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: [],
+    })
+  }
+
+  const getUser = async (name) => {
+    setLoading()
+
+    //axios
+    dispatch({
+      type: GET_USER,
+      payload: {},
+    })
+  }
+
+  const getRepos = async (name) => {
+    setLoading()
+
+    //axios
+    dispatch({
+      type: GET_REPOS,
+      payload: [],
+    })
+  }
+
+  const clearUsers = () => dispatch({ type: CLEAR_USERS })
+
+  const setLoading = () => dispatch({ type: SET_LOADING })
+
+  const { user, users, repos, loading } = state
+
+  retunr(
+    <GithubContext.Provider
+      value={{
+        getRepos,
+        getUser,
+        search,
+        clearUsers,
+        setLoading,
+        user,
+        users,
+        repos,
+        loading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
 }
